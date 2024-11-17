077c0c07a5ed0fa3e1d6814e16dd3535f37acc44
sarbajit1000 <you@example.com>
2024-11-17 12:42:17 +0530

    new

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect 
 from django.shortcuts import render 
 from .form import ReviewForm
-from .models import Review
+from django.views.generic.base import TemplateView
 from django.views import View
 # Create your views here.
 
@@ -18,19 +18,11 @@ class ReviewView(View):
             return HttpResponseRedirect("/thank_you")
         return render(request, "reviews/review.html", {"form":form})
 
-# def reviews(request):
-#     if request.method == 'POST':
-        
-#         form = ReviewForm(request.POST, )
-        
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect("/thank_you")
-#     else:
-#         form = ReviewForm()
+class ThankYouView(TemplateView):
+    # def get(self, request):
+    #     return render(request, "reviews/thankyou.html")
+    template_name = "reviews/thankyou.html"
 
-#     return render(request, "reviews/review.html", {"form":form})
 
-def thank_you(request):
-    return render(request, "reviews/thankyou.html", {"has_error": True})
+    
     
